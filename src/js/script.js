9534fdc002f4e541bf456d02cc6488ff84ff83ed
j-tap <j-tap@ya.ru>
2019-07-11 03:34:31 +0300

    create form questionary

--- src/js/script.js
+++ src/js/script.js
@@ -58,7 +58,72 @@ $(document).ready(() => {
 		scrlLoad()
 	})
 
+	tabsDefaultActive()
+
+	$('.questionary-form').each(function () {
+		new QuestionaryForm($(this))
+	})
 })
+
+function QuestionaryForm (form) {
+	this.curTab = 0
+	this.$form = form
+	this.$prog = this.$form.find('.progress-bar')
+	this.$complete = this.$form.next('.questionary-complete')
+	this.$tabs = this.$form.find('.nav-tabs').find('.nav-link')
+	this.$votes = this.$form.find('.tab-pane').find('.form-group')
+	this.cntTabs = this.$tabs.length
+
+	this.$tabs.slice(1).attr('aria-disabled', true).addClass('disabled') // block all after first
+
+	this.$votes.find('.custom-control-input').on('change', () => {
+		if (this.curTab === this.$tabs.filter('.active').parent().index()) { // check active tab is current index
+			this.next()
+		}
+		else {
+			this.goCurIndex()
+		}
+	})
+
+	this.next = function () {
+		const percent = (this.curTab + 1) * (100 / this.cntTabs)
+		this.$tabs.eq(this.curTab).addClass('complete')
+
+		if (this.$tabs.eq(this.curTab + 1).length) {
+			this.$tabs.eq(this.curTab + 1).removeAttr('aria-disabled').removeClass('disabled').tab('show')
+			this.curTab++
+		}
+		else {
+			this.$tabs.eq(this.curTab).removeClass('active')
+			this.sendForm()
+		}
+
+		this.$prog.css('width', `${percent}%`)
+	}
+
+	this.goCurIndex = function () {
+		this.$tabs.eq(this.curTab).tab('show')
+	}
+
+	this.sendForm = function () {
+		this.$form.fadeOut(200, () => {
+			this.$complete.fadeIn(200)
+		})
+	}
+}
+
+function tabsDefaultActive (){
+	$('.nav-tabs').each(function () {
+		if (!$(this).find('.nav-link.active').length) {
+			$(this).children().first().children().addClass('active')
+		}
+	})
+	$('.tab-content').each(function () {
+		if (!$(this).children('.tab-pane.show.active').length) {
+			$(this).children().first().addClass('active show')
+		}
+	})
+}
 
 function featherReplaceAndInit () {
 	$('span.feather').each(function () {
